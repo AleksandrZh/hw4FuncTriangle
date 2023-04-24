d785a894959c676b7485d05c8fb79d9fa9216860
ZhmakinAA <ZhmakinAA@hotmail.ru>
2023-04-24 16:43:36 +0300

    Добавьте файлы проекта.

---
+++ Operation.cs
@@ -0,0 +1,43 @@
+﻿namespace hw4FuncTriangle
+{
+    internal class Operation
+    {
+
+        private static bool CheckForExistenceTriangle(double a, double b, double c)
+        {
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static double CalculateAreaTriangle(double a, double b, double c)
+        {
+
+            if (!CheckForExistenceTriangle(a, b, c))
+                return 0;
+
+            double p = ((a + b + c) / 2);
+
+            double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+            return s;
+
+        }
+
+        public static double CalculateAreaTriangle(double a)
+        {
+
+            double p = ((3 * a) / 2);
+            double s = Math.Sqrt(p * Math.Pow((p - a), 2));
+            return s;
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿namespace hw4FuncTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool flagExistence;
+            int type;
+            double a, b, c;
+            Console.WriteLine($"Введите тип треугольника (1 - равтосторонний; 2 - не равносторонний)");
+            type = int.Parse(Console.ReadLine());
+
+            ;
+
+
+            if (type == 2)
+            {
+
+
+                Console.WriteLine("a:");
+                a = double.Parse(Console.ReadLine());
+                Console.WriteLine("b:");
+                b = double.Parse(Console.ReadLine());
+                Console.WriteLine("c:");
+                c = double.Parse(Console.ReadLine());
+
+                flagExistence = (Operation.CalculateAreaTriangle(a, b, c) == 0);
+
+
+                if (flagExistence)
+                {
+                    Console.WriteLine("Такого треугольника не существует, расчитать площадь невозможно");
+                }
+                else
+                {
+                    Console.WriteLine($"Площадь треугольника: {Operation.CalculateAreaTriangle(a, b, c)}");
+                }
+
+
+
+
+
+            }
+            else if (type == 1)
+            {
+                Console.WriteLine("a:");
+                a = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Площадь треугольника: {Operation.CalculateAreaTriangle(a)}");
+
+            }
+            else
+            {
+                Console.WriteLine("Введено не верное заначение. До встречи!");
+            }
+
+
+
+        }
+    }
+}
